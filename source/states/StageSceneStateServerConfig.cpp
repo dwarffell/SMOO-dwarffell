ddd2ee241d3e94b97774b41d48bd055274fd8737
Robin C. Ladiges <rcl.git@blackpinguin.de>
2024-07-09 09:01:08 +0200

    fix: check Ryujinx default profile only on emulators

--- source/states/StageSceneStateServerConfig.cpp
+++ source/states/StageSceneStateServerConfig.cpp
@@ -110,7 +110,7 @@ StageSceneStateServerConfig::StageSceneStateServerConfig(const char *name, al::S
 void StageSceneStateServerConfig::init() {
     initNerve(&nrvStageSceneStateServerConfigMainMenu, 0);
 
-    #ifdef EMU
+    #if EMU
     char ryujinx[0x10] = { 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
     nn::account::Uid user;
     nn::account::GetLastOpenedUser(&user);
