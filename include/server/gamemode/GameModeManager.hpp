CraftyBoss/SuperMarioOdysseyOnline

commit 34691affb0d0fa3e42f39e524d946b5b83b782b9

Overhaul gamemode management

include/server/gamemode/GameModeManager.hpp
@@ -1,0 +1,59 @@
+#pragma once
+
+#include <heap/seadDisposer.h>
+#include <heap/seadHeap.h>
+#include <container/seadSafeArray.h>
+#include "server/gamemode/GameModeBase.hpp"
+#include "server/gamemode/GameModeInfoBase.hpp"
+
+class GameModeManager {
+    SEAD_SINGLETON_DISPOSER(GameModeManager)
+    GameModeManager();
+    ~GameModeManager();
+
+public:
+    void setMode(GameMode mode);
+    void initScene(const GameModeInitInfo& info);
+    void begin();
+    void end();
+    void update();
+
+    GameMode getGameMode() const { return mCurMode; }
+    template<class T> T* getMode() const { return static_cast<T*>(mCurModeBase); }
+    template<class T> T* getInfo() const { return static_cast<T*>(mModeInfo); }
+    void setInfo(GameModeInfoBase* info) {
+        mModeInfo = info;
+    }
+
+    template<class T>
+    T* createModeInfo();
+
+    sead::Heap* getHeap() { return mHeap; }
+    void toggleActive();
+    void setActive(bool active) { mActive = active; }
+    void setPaused(bool paused);
+    bool isMode(GameMode mode) const { return mCurMode == mode; }
+    bool isActive() const { return mActive; }
+    bool isModeAndActive(GameMode mode) const { return isMode(mode) && isActive(); }
+    bool isPaused() const { return mPaused; }
+private:
+    sead::Heap* mHeap = nullptr;
+
+    bool mActive = false;
+    bool mPaused = false;
+    bool mWasSceneTrans = false;
+    bool mWasSetMode = false;
+    GameMode mCurMode = GameMode::NONE;
+    GameModeBase* mCurModeBase = nullptr;
+    GameModeInfoBase *mModeInfo = nullptr;
+    GameModeInitInfo *mLastInitInfo = nullptr;
+};
+
+template<class T>
+T* GameModeManager::createModeInfo() {
+    sead::ScopedCurrentHeapSetter heapSetter(mHeap);
+
+    T* info = new T();
+    mModeInfo = info;
+    return info;
+}

MakefileNSO
@@ -32,7 +32,7 @@ include $(DEVKITPRO)/libnx/switch_rules
 #---------------------------------------------------------------------------------
 TARGET		?=	$(notdir $(CURDIR))$(SMOVER)
 BUILD		?=	build$(SMOVER)
-SOURCES		:=  source/sead/time source/sead source/puppets source/server source/layouts source/states source/cameras source/nx source
+SOURCES		:=  source/sead/time source/sead source/puppets source/server/hns source/server/gamemode source/server source/layouts source/states source/cameras source/nx source
 DATA		:=	data
 INCLUDES	:=	include include/sead
 
@@ -46,7 +46,7 @@ CFLAGS	:=	-g -Wall -ffunction-sections \
 
 CFLAGS	+=	$(INCLUDE) -D__SWITCH__ -DSMOVER=$(SMOVER) -O3 -DNNSDK -DSWITCH -DBUILDVERSTR=$(BUILDVERSTR) -DBUILDVER=$(BUILDVER) -DDEBUGLOG=$(DEBUGLOG) -DSERVERIP=$(SERVERIP) -DEMU=$(EMU)
 
-CXXFLAGS	:= $(CFLAGS) -fno-rtti -fomit-frame-pointer -fno-exceptions -fno-asynchronous-unwind-tables -fno-unwind-tables -std=gnu++20
+CXXFLAGS	:= $(CFLAGS) -Wno-invalid-offsetof -Wno-volatile -fno-rtti -fomit-frame-pointer -fno-exceptions -fno-asynchronous-unwind-tables -fno-unwind-tables -std=gnu++20
 
 ASFLAGS	:=	-g $(ARCH)
 LDFLAGS  =  -specs=../switch.specs -g $(ARCH) -Wl,-Map,$(notdir $*.map) -Wl,--version-script=$(TOPDIR)/exported.txt -Wl,-init=__custom_init -Wl,-fini=__custom_fini -nostdlib

include/al/util.hpp
@@ -72,6 +72,9 @@ namespace al
     PlayerActorBase *tryGetPlayerActor(al::PlayerHolder const *, int);
 
     sead::Heap *getCurrentHeap(void);
+    sead::Heap* getStationedHeap();
+    sead::Heap* getSequenceHeap();
+    sead::Heap* getSceneHeap();
 
     al::Projection *getProjection(al::IUseCamera const *, int);
 

include/game/StageScene/StageSceneStateServerConfig.hpp
@@ -17,6 +17,7 @@
 
 #include "logger.hpp"
 #include "server/gamemode/GameModeConfigMenu.hpp"
+#include "server/gamemode/GameModeConfigMenuFactory.hpp"
 
 class FooterParts;
 
@@ -65,14 +66,18 @@ class StageSceneStateServerConfig : public al::HostStateBase<al::Scene>, public 
         // Root Page, contains buttons for gamemode config, server reconnecting, and server ip address changing
         SimpleLayoutMenu* mMainOptions = nullptr;
         CommonVerticalList *mMainOptionsList = nullptr;
-        // Sub-Page for Mode configuration, has buttons for selecting current gamemode and configuring currently selected mode (if no mode is chosen, button will not do anything)
-        SimpleLayoutMenu* mGamemodeConfig = nullptr;
-        CommonVerticalList* mGameModeConfigList = nullptr;
         // Sub-Page of Mode config, used to select a gamemode for the client to use
         SimpleLayoutMenu* mModeSelect = nullptr;
         CommonVerticalList* mModeSelectList = nullptr;
-        // Controls what shows up in specific gamemode config page
-        GameModeConfigMenu *mGamemodeConfigMenu = nullptr;
+
+        // Sub-Pages for Mode configuration, has buttons for selecting current gamemode and configuring currently selected mode (if no mode is chosen, button will not do anything)
+        struct GameModeEntry {
+            GameModeConfigMenu* mMenu;
+            SimpleLayoutMenu* mLayout = nullptr;
+            CommonVerticalList* mList = nullptr;
+        };
+        sead::SafeArray<GameModeEntry, GameModeConfigMenuFactory::getMenuCount()> mGamemodeConfigMenus;
+        GameModeEntry *mGamemodeConfigMenu = nullptr;
 
         bool mIsDecideConfig = false;
 };

include/server/Client.hpp
@@ -96,8 +96,6 @@ class Client {
         static bool isNeedUpdateShines();
         bool isShineCollected(int shineId);
 
-        static void initMode(GameModeInitInfo const &initInfo);
-
         static void sendHackCapInfPacket(const HackCap *hackCap);
         static void sendPlayerInfPacket(const PlayerActorHakoniwa *player);
         static void sendGameInfPacket(const PlayerActorHakoniwa *player, GameDataHolderAccessor holder);
@@ -109,8 +107,6 @@ class Client {
 
         int getCollectedShinesCount() { return curCollectedShines.size(); }
         int getShineID(int index) { if (index < curCollectedShines.size()) { return curCollectedShines[index]; } return -1; }
-
-        static void setGameActive(bool state);
 
         static void setStageInfo(GameDataHolderAccessor holder);
 
@@ -130,26 +126,7 @@ class Client {
 
         static PuppetActor* getDebugPuppet();
 
-        static GameMode getServerMode() {
-            return sInstance ? sInstance->mServerMode : GameMode::NONE;
-        }
-
-        static void setServerMode(GameMode mode) {
-            if (sInstance) sInstance->mServerMode = mode;
-        }
-
-        static GameMode getCurrentMode();
-
-        static GameModeBase* getModeBase() { return sInstance ? sInstance->mCurMode : nullptr; }
-
-        template <typename T>
-        static T* getMode() {return sInstance ? (T*)sInstance->mCurMode : nullptr;}
-
-        static GameModeConfigMenu* tryCreateModeMenu();
-
         static int getMaxPlayerCount() { return sInstance ? sInstance->maxPuppets + 1 : 10;}
-
-        static void toggleCurrentMode();
 
         static void updateStates();
 
@@ -173,6 +150,12 @@ class Client {
             return 0;
         }
 
+        static PuppetHolder* getPuppetHolder() {
+            if (sInstance)
+                return sInstance->mPuppetHolder;
+            return nullptr;
+        }
+
         static void setSceneInfo(const al::ActorInitInfo& initInfo, const StageScene *stageScene);
 
         static bool tryRegisterShine(Shine* shine);
@@ -183,23 +166,6 @@ class Client {
 
         static bool openKeyboardIP();
         static bool openKeyboardPort();
-
-        static GameModeInfoBase* getModeInfo() {
-            return sInstance ? sInstance->mModeInfo : nullptr;
-        }
-
-        // should only be called during mode init
-        static void setModeInfo(GameModeInfoBase* info) {
-            if(sInstance) sInstance->mModeInfo = info;
-        }
-
-        static void tryRestartCurrentMode();
-
-        static bool isModeActive() { return sInstance ? sInstance->mIsModeActive : false; }
-
-        static bool isSelectedMode(GameMode mode) {
-            return sInstance ? sInstance->mCurMode->getMode() == mode : false;
-        }
 
         static void showConnect();
 
@@ -295,21 +261,11 @@ class Client {
 
         sead::FrameHeap *mHeap = nullptr; // Custom FrameHeap used for all Client related memory
 
-        // --- Mode Info ---
-
-        GameModeBase* mCurMode = nullptr;
-
-        GameMode mServerMode = GameMode::NONE; // current mode set by server, will sometimes not match up with current game mode (until scene re-init) if server switches gamemodes
-
-        GameModeInfoBase *mModeInfo = nullptr;
-
-        bool mIsModeActive = false;
-
         // --- Puppet Info ---
 
         int maxPuppets = 9;  // default max player count is 10, so default max puppets will be 9
         
-        PuppetInfo *mPuppetInfoArr[MAXPUPINDEX];
+        PuppetInfo *mPuppetInfoArr[MAXPUPINDEX] = {};
 
         PuppetHolder *mPuppetHolder = nullptr;
 

include/server/gamemode/GameModeBase.hpp
@@ -21,7 +21,6 @@ enum GameMode : s8 {
 
 // struct containing info about the games state for use in gamemodes
 struct GameModeInitInfo {
-
     GameModeInitInfo(al::ActorInitInfo* info, al::Scene *scene){
         mLayoutInitInfo = info->mLayoutInitInfo;
         mPlayerHolder = info->mActorSceneInfo.mPlayerHolder;
@@ -47,6 +46,7 @@ struct GameModeInitInfo {
 class GameModeBase : public al::IUseName, public al::IUseSceneObjHolder {
 public:
     GameModeBase(const char* name) { mName = name; }
+    virtual ~GameModeBase() = default;
     const char* getName() const override { return mName.cstr(); }
     al::SceneObjHolder* getSceneObjHolder() const override { return mSceneObjHolder; }
 
@@ -56,7 +56,7 @@ public:
 
     virtual void init(GameModeInitInfo const &info);
 
-    virtual void begin() { mIsActive = true;}
+    virtual void begin() { mIsActive = true; }
     virtual void update();
     virtual void end() { mIsActive = false; }
     

include/server/gamemode/GameModeConfigMenuFactory.hpp
@@ -1,0 +1,38 @@
+#pragma once
+
+#include "al/factory/Factory.h"
+#include "server/hns/HideAndSeekConfigMenu.hpp"
+#include "server/gamemode/GameModeConfigMenu.hpp"
+
+typedef GameModeConfigMenu* (*createMenu)(const char* name);
+
+template <class T>
+GameModeConfigMenu* createGameModeConfigMenu(const char* name) {
+    return new T();
+};
+
+__attribute((used)) constexpr al::NameToCreator<createMenu> menuTable[] = {
+    {"HideAndSeek", &createGameModeConfigMenu<HideAndSeekConfigMenu>},
+};
+
+class GameModeConfigMenuFactory : public al::Factory<createMenu> {
+public:
+    GameModeConfigMenuFactory(const char* fName) {
+        this->factoryName = fName;
+        this->actorTable = menuTable;
+        this->factoryCount = sizeof(menuTable) / sizeof(menuTable[0]);
+    };
+
+    constexpr static const char* getMenuName(int idx);
+    constexpr static int getMenuCount();
+};
+
+constexpr const char* GameModeConfigMenuFactory::getMenuName(int idx) {
+    if (idx >= 0 && idx < sizeof(menuTable) / sizeof(menuTable[0]))
+        return menuTable[idx].creatorName;
+    return nullptr;
+}
+
+constexpr int GameModeConfigMenuFactory::getMenuCount() {
+    return sizeof(menuTable) / sizeof(menuTable[0]);
+}

include/server/gamemode/GameModeFactory.hpp
@@ -1,9 +1,8 @@
 #pragma once
 
 #include "al/factory/Factory.h"
-#include "layouts/HideAndSeekIcon.h"
 #include "server/gamemode/GameModeBase.hpp"
-#include "server/HideAndSeekMode.hpp"
+#include "server/hns/HideAndSeekMode.hpp"
 
 typedef GameModeBase* (*createMode)(const char* name);
 
@@ -37,19 +36,19 @@ class GameModeFactory : public al::Factory<createMode> {
 
 // TODO: possibly use shadows' crc32 hash algorithm for this
 constexpr const char* GameModeFactory::getModeString(GameMode mode) {
-    if(mode >= 0 && mode < sizeof(modeTable)/sizeof(modeTable[0]))
+    if(mode >= 0 && (size_t)mode < sizeof(modeTable)/sizeof(modeTable[0]))
         return modeTable[mode].creatorName;
     return nullptr;
 }
 
 constexpr const char* GameModeFactory::getModeName(GameMode mode) {
-    if(mode >= 0 && mode < sizeof(modeNames)/sizeof(modeNames[0]))
+    if(mode >= 0 && (size_t)mode < sizeof(modeNames)/sizeof(modeNames[0]))
         return modeNames[mode];
     return nullptr;
 }
 
 constexpr const char* GameModeFactory::getModeName(int idx) {
-    if(idx >= 0 && idx < sizeof(modeNames)/sizeof(modeNames[0]))
+    if(idx >= 0 && (size_t)idx < sizeof(modeNames)/sizeof(modeNames[0]))
         return modeNames[idx];
     return nullptr;
 }

dwarffell/SMOO-dwarffell

commit d3dcc7e510fd0b45a085692856a1aaa5bf91bc5d

fix: don't crash when changing the language (only when offline)
This change only makes it work when in offline/airplane mode.
When connected to a server it still crashes.
Likely due to a different part of the code.

Thanks to https://github.com/piplup55 for pointing out to me that this worked in old versions.
I then tested a few older builds and found out that it started to not work with version `v1.1.0`.
Using `git bisect` I was able to find out that commit 34691affb0d0fa3e42f39e524d946b5b83b782b9 introduced the issue.

include/server/gamemode/GameModeManager.hpp
@@ -11,7 +11,6 @@
 class GameModeManager {
     SEAD_SINGLETON_DISPOSER(GameModeManager)
     GameModeManager();
-    ~GameModeManager();
 
 public:
     void setMode(GameMode mode);